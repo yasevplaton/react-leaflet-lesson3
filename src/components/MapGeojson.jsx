8baa12625e63bac7e9508aa5ba9aa341d01cc707
yasevplaton <yasevplaton@gmail.com>
2020-02-25 14:53:36 +0300

    add more markers; change their icons

--- src/components/MapGeojson.jsx
+++ src/components/MapGeojson.jsx
@@ -1,45 +1,90 @@
 import React from "react";
-import L from 'leaflet';
+import L from "leaflet";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-import Basemap from './Basemaps';
-import GeojsonLayer from './GeojsonLayer';
-import '../css/Map.css';
+import Basemap from "./Basemaps";
+import GeojsonLayer from "./GeojsonLayer";
+import "../css/Map.css";
 
 // указываем путь к файлам marker
-L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
+// L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
+
+// you can set .my-div-icon styles in CSS
+// L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
 
 class MapComponent extends React.Component {
   state = {
     lat: 55.702868,
     lng: 37.530865,
     zoom: 10,
-    basemap: 'osm',
+    basemap: "osm",
+    markers: [
+      {
+        id: 1,
+        coordinates: [37.5677490234375, 55.67835873246176]
+      },
+      {
+        id: 2,
+        coordinates: [37.662506103515625, 55.584554519645074]
+      },
+      {
+        id: 3,
+        coordinates: [37.77923583984374, 55.60085069621599]
+      },
+      {
+        id: 4,
+        coordinates: [37.843780517578125, 55.68455275165637]
+      },
+      {
+        id: 5,
+        coordinates: [37.8204345703125, 55.713960948980805]
+      },
+      {
+        id: 6,
+        coordinates: [37.6995849609375, 55.72401656896143]
+      },
+      {
+        id: 7,
+        coordinates: [37.577362060546875, 55.72401656896143]
+      },
+      {
+        id: 8,
+        coordinates: [37.77923583984374, 55.66441860246834]
+      },
+      {
+        id: 9,
+        coordinates: [37.5677490234375, 55.67835873246176]
+      },
+      {
+        id: 10,
+        coordinates: [37.530865, 55.702868]
+      }
+    ],
 
-    geojsonvisible: false,
+    geojsonvisible: false
   };
 
-  onBMChange = (bm) => {
+  onBMChange = bm => {
     this.setState({
       basemap: bm
     });
-  }
+  };
 
-  onGeojsonToggle = (e) => {
-    
+  onGeojsonToggle = e => {
     this.setState({
       geojsonvisible: e.currentTarget.checked
     });
-  }
+  };
 
   render() {
+    const circleIcon = L.divIcon({ className: "my-div-icon" });
     var center = [this.state.lat, this.state.lng];
 
     const basemapsDict = {
       osm: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       hot: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-      dark:"https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
+      dark: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
       cycle: "https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
-    }
+    };
 
     return (
       <Map zoom={this.state.zoom} center={center}>
@@ -51,21 +96,27 @@ class MapComponent extends React.Component {
 
         <div className="geojson-toggle">
           <label htmlFor="layertoggle">Toggle Geojson </label>
-          <input type="checkbox"
-            name="layertoggle" id="layertoggle"
-            value={this.state.geojsonvisible} onChange={this.onGeojsonToggle} />
+          <input
+            type="checkbox"
+            name="layertoggle"
+            id="layertoggle"
+            value={this.state.geojsonvisible}
+            onChange={this.onGeojsonToggle}
+          />
         </div>
-        
-        {this.state.geojsonvisible && 
-          <GeojsonLayer url="geojson.json" />
-        }
 
-        <Marker position={center}>
-          <Popup>Выбрана тема {this.state.basemap}</Popup>
-        </Marker>
+        {this.state.geojsonvisible && <GeojsonLayer url="geojson.json" />}
+
+        {this.state.markers.map(f => {
+          return (
+            <Marker position={[f.coordinates[1], f.coordinates[0]]} key={f.id} icon={circleIcon}>
+              <Popup>{f.id}</Popup>
+            </Marker>
+          );
+        })}
       </Map>
     );
   }
-};
+}
 
 export default MapComponent;
